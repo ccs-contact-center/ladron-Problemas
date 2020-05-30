d4a3d28f3f4c0c3537a16e63f748a59f691a9034
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-05-30 13:36:22 -0500

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -12,41 +12,23 @@ export default {
     },
 
     {
-      name: '¿QUE ES LA EMPATIA?',
+      name: 'DETECCIÓN DE PROBLEMAS ',
       icon: 'icon-tag',
       
     },
 
     {
-      name: 'TIPOS DE EMPATIA',
+      name: 'PRIORIZANDO PROBLEMAS  ',
       icon: 'icon-tag',
      
     },
     {
-      name: 'RASGOS DE UN INDIVIDUO EMPATICO',
+      name: 'BUSCANDO UNA SOLUCIÓN ',
       icon: 'icon-tag',
      
     },
-    {
-      name: 'DESARROLLO DE LA EMPATIA ',
-      icon: 'icon-tag',
-     
-    },
-    {
-      name: 'OBSTACULOS DE LA EMPATIA',
-      icon: 'icon-tag',
-     
-    },
-    {
-      name: 'MEJORAS DE LA EMPATIA',
-      icon: 'icon-tag',
-     
-    },
-    {
-      name: 'CONCLUCIÓN',
-      icon: 'icon-tag',
-     
-    },
+    
+    
     {
       name: 'EVALUACIÓN ',
       icon: 'icon-puzzle',
